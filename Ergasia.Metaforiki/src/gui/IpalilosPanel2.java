53f347ccf309d6866a9195c6c8204b78f47308ae
Fotis <Fotis@Desktop>
2013-12-21 15:10:16 +0200

    IpalilosPanel2 grafei sti vasi xwris elegxous sta textboxes

--- Ergasia.Metaforiki/src/gui/IpalilosPanel2.java
+++ Ergasia.Metaforiki/src/gui/IpalilosPanel2.java
@@ -3,7 +3,9 @@ package gui;
 import javax.swing.JPanel;
 import javax.swing.BoxLayout;
 import javax.swing.JTabbedPane;
+
 import net.miginfocom.swing.MigLayout;
+
 import javax.swing.JLabel;
 import javax.swing.JFormattedTextField;
 import javax.swing.JCheckBox;
@@ -11,10 +13,24 @@ import javax.swing.JTextArea;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
+
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Random;
+
 
 public class IpalilosPanel2 extends JPanel {
-
+	
+	public static java.sql.Date getCurrentJavaSqlDate() {
+	    java.util.Date today = new java.util.Date();
+	    return new java.sql.Date(today.getTime());
+	  }
 	private static final long serialVersionUID = 1;
 
 	public IpalilosPanel2() {
@@ -34,44 +50,46 @@ public class IpalilosPanel2 extends JPanel {
 		JLabel lblName = new JLabel("Name");
 		panel.add(lblName, "cell 0 0,alignx center,aligny center");
 		
-		JFormattedTextField formattedTextField = new JFormattedTextField();
-		panel.add(formattedTextField, "cell 1 0 2 1,grow");
+		final JFormattedTextField frmtdtxtfldAsdsd = new JFormattedTextField();
+		panel.add(frmtdtxtfldAsdsd, "cell 1 0 2 1,grow");
 		
 		JLabel lblSurname = new JLabel("Surname");
 		panel.add(lblSurname, "cell 0 1,alignx center,aligny center");
 		
-		JFormattedTextField formattedTextField_1 = new JFormattedTextField();
-		panel.add(formattedTextField_1, "cell 1 1 2 1,grow");
+		final JFormattedTextField frmtdtxtfldSdfafdsf = new JFormattedTextField();
+		panel.add(frmtdtxtfldSdfafdsf, "cell 1 1 2 1,grow");
 		
 		JLabel lblAddress = new JLabel("Address");
 		panel.add(lblAddress, "cell 0 2,alignx center,aligny center");
 		
-		JFormattedTextField formattedTextField_2 = new JFormattedTextField();
-		panel.add(formattedTextField_2, "cell 1 2 2 1,grow");
+		final JFormattedTextField frmtdtxtfldDfsgsdfsdfasf = new JFormattedTextField();
+		panel.add(frmtdtxtfldDfsgsdfsdfasf, "cell 1 2 2 1,grow");
 		
 		JLabel lblPostalCode = new JLabel("Postal Code");
 		panel.add(lblPostalCode, "cell 0 3,alignx center,aligny center");
 		
-		JFormattedTextField formattedTextField_3 = new JFormattedTextField();
+		final JFormattedTextField formattedTextField_3 = new JFormattedTextField();
 		panel.add(formattedTextField_3, "cell 1 3 2 1,grow");
 		
 		JLabel lblCountry = new JLabel("Country");
 		panel.add(lblCountry, "cell 0 4,alignx center,aligny center");
 		
-		JFormattedTextField formattedTextField_4 = new JFormattedTextField();
-		panel.add(formattedTextField_4, "cell 1 4 2 1,grow");
+		final JFormattedTextField frmtdtxtfldFdssdfg = new JFormattedTextField();
+		panel.add(frmtdtxtfldFdssdfg, "cell 1 4 2 1,grow");
 		
 		JLabel lblPhoneNumber = new JLabel("Phone Number");
 		panel.add(lblPhoneNumber, "cell 0 5,alignx center,aligny center");
 		
-		JFormattedTextField formattedTextField_5 = new JFormattedTextField();
+		final JFormattedTextField formattedTextField_5 = new JFormattedTextField();
+		formattedTextField_5.setText("");
 		panel.add(formattedTextField_5, "cell 1 5 2 1,grow");
 		
 		JLabel lblFragile = new JLabel("Fragile");
 		panel.add(lblFragile, "cell 0 6,alignx center,aligny center");
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("");
+		final JCheckBox chckbxNewCheckBox = new JCheckBox("");
 		panel.add(chckbxNewCheckBox, "cell 1 6");
+		
 		
 		JLabel lblComments = new JLabel("Comments");
 		panel.add(lblComments, "cell 0 7,alignx center,aligny center");
@@ -80,16 +98,94 @@ public class IpalilosPanel2 extends JPanel {
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		panel.add(scrollPane, "cell 1 7 2 1,grow");
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setFont(new Font("Monospaced", Font.PLAIN, 11));
-		textArea.setLineWrap(true);
-		scrollPane.setViewportView(textArea);
+		final JTextArea txtrDfcsbnfhgGhetrgGh = new JTextArea();
+		txtrDfcsbnfhgGhetrgGh.setFont(new Font("Monospaced", Font.PLAIN, 11));
+		txtrDfcsbnfhgGhetrgGh.setLineWrap(true);
+		scrollPane.setViewportView(txtrDfcsbnfhgGhetrgGh);
+		
+		
 		
 		JButton btnClear = new JButton("Clear");
 		panel.add(btnClear, "cell 1 8,alignx left,growy");
 		
+		
+		/*
+		final String name = frmtdtxtfldAsdsd.getText(); //Name
+		final String surname = frmtdtxtfldSdfafdsf.getText(); //Surname
+		final String address = frmtdtxtfldDfsgsdfsdfasf.getText(); //Address
+		final int tk = Integer.parseInt(formattedTextField_3.getText()); //Postal Code
+		final String xwra = frmtdtxtfldFdssdfg.getText(); //Country
+		final int phone = Integer.parseInt(formattedTextField_5.getText()); // Phone
+		final boolean checkbox = chckbxNewCheckBox.isSelected();//checkbox
+		final String comment = txtrDfcsbnfhgGhetrgGh.getText(); //Comment
+		*/
+		
+		
 		JButton btnSubmit = new JButton("Submit");
 		panel.add(btnSubmit, "cell 2 8,alignx right,growy");
+		btnSubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				try{
+					
+					
+					//elegxos gia tixon null sta Jtextboxes
+					//grafw sti vasi
+					Class.forName("com.mysql.jdbc.Driver").newInstance();
+					
+					String url = "jdbc:mysql://83.212.112.80/foo";
+					String username = "root";
+					String pass = "1234";
+					
+					Connection conn = DriverManager.getConnection(url, username, pass);
+					Statement stm = conn.createStatement();
+					
+					int id2=15;
+					String sqldiavasma = "select * from demata";
+					ResultSet rs = stm.executeQuery(sqldiavasma);
+					while(rs.next()){
+						id2 = rs.getInt("id");
+						}
+					
+					
+					//tyxaio tracking number Elegxos me epanalipsi an yparxei eidh
+					int min = 0000000;
+					int max = 9999999;
+					Random r = new Random();
+					int trackingnumber = r.nextInt(max - min + 1) + min;
+					
+					//Arxiki katastasi (Mi paradomeno)
+					int katastasi = 0;
+					
+					//fragile
+					int ckeck = 0;
+					if (chckbxNewCheckBox.isSelected()) {
+						ckeck = 1;
+						}
+					String sqlgrapsimo = "insert into demata values(?,?,?,?,?,?,?,?,?,?,?,?)";
+					PreparedStatement ps = conn.prepareStatement(sqlgrapsimo);
+					System.out.println(id2 + 1);
+					ps.setInt(1, id2 + 1);
+					ps.setInt(2, trackingnumber);
+					ps.setString(3, frmtdtxtfldAsdsd.getText());
+					ps.setString(4, frmtdtxtfldSdfafdsf.getText());
+					ps.setString(5, frmtdtxtfldDfsgsdfsdfasf.getText());
+					ps.setInt(6, Integer.parseInt(formattedTextField_3.getText()));
+					ps.setString(7, frmtdtxtfldFdssdfg.getText());
+					ps.setInt(8, Integer.parseInt(formattedTextField_5.getText()));
+					ps.setInt(9, ckeck);
+					ps.setString(10, txtrDfcsbnfhgGhetrgGh.getText());
+					ps.setInt(11, katastasi);
+					java.sql.Date date = getCurrentJavaSqlDate();
+				    ps.setDate(12, date);
+					ps.executeUpdate();
+					
+					
+					}catch(Exception e){
+				e.printStackTrace();
+			}}
+			});
+		
 		
 		/* 
 		 * Tab 2
