34cf944477acf7e5be6a926b1fa034f4e0a4498c
Duduris <Duduris@gmail.com>
2013-12-26 18:04:10 +0200

    Main Panel gia na ginete epilogi xristi - to user name kai password den kanoun tpt gia tora.

---
+++ Ergasia.Metaforiki/src/additional/MainPanel.java
@@ -0,0 +1,86 @@
+package additional;
+
+import gui.AdministratorPanel;
+import gui.CustomerPanel;
+import gui.IpalilosPanel;
+import gui.LoginPanel;
+import gui.MetaforeasPanel;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JButton;
+
+import controllers.AdministratorController;
+import controllers.CustomerController;
+import controllers.IpalilosController;
+import controllers.LoginController;
+import controllers.MetaforeasController;
+
+import java.awt.CardLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MainPanel {
+	
+	private static CardLayout card;
+	private static JMenuBar menuBar;
+	private static JButton btnNewButton;
+
+	public static void main(String[] args) {
+		
+		  final JFrame frame = new JFrame("Metaforiki");
+		  
+		  Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); //diavazei tin analisi tis othonis
+		  
+		  frame.setBounds(0,0,(int) (screenSize.width/2.5), screenSize.height/2); //orizei to megethos tou parathirou sto /2.5 tis analisis y kai /3 sto x
+		  frame.setLocation((screenSize.width - frame.getSize().width)/2 , (screenSize.height - frame.getSize().height)/2); //to vazei sto kendro tis othonis
+		  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		  
+		  menuBar = new JMenuBar();
+		  frame.setJMenuBar(menuBar);
+		  
+		  btnNewButton = new JButton("Back");
+		  menuBar.add(btnNewButton);
+		  frame.getContentPane().setLayout(new CardLayout(0, 0));
+		  
+		  LoginPanel login = new LoginPanel();
+		  IpalilosPanel ipalilos = new IpalilosPanel();
+		  MetaforeasPanel metaforeas = new MetaforeasPanel();
+		  CustomerPanel customer = new CustomerPanel();
+		  AdministratorPanel admin = new AdministratorPanel();
+		  
+		  final LoginController logcont = new LoginController(login);
+		  IpalilosController ipalcont = new IpalilosController(ipalilos);
+		  MetaforeasController metcont = new MetaforeasController(metaforeas);
+		  CustomerController custcont = new CustomerController(customer);
+		  AdministratorController admincont = new AdministratorController(admin);
+		  
+		  
+		  frame.getContentPane().add(login, "Log");
+		  frame.getContentPane().add(ipalilos, "Clerk");
+		  frame.getContentPane().add(metaforeas, "Courier");
+		  frame.getContentPane().add(customer, "Customer");
+		  frame.getContentPane().add(admin, "Administrator");
+		  
+		  card = (CardLayout) frame.getContentPane().getLayout();
+		  card.show(frame.getContentPane(), "Login");
+		  
+		  frame.setVisible(true);
+		  
+		  btnNewButton.addActionListener(new ActionListener() {
+			  public void actionPerformed(ActionEvent e) {
+				  card.show(frame.getContentPane(), "Log");
+			  }
+		  });
+		  login.btnSingIn.addActionListener(new ActionListener() {
+			  public void actionPerformed(ActionEvent arg0) {
+				  card.show(frame.getContentPane(), logcont.changePanel());
+			  }
+		  });
+
+	}
+
+}
--- Ergasia.Metaforiki/src/controllers/LoginController.java
+++ Ergasia.Metaforiki/src/controllers/LoginController.java
@@ -1,7 +1,4 @@
 package controllers;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import gui.LoginPanel;
 
 public class LoginController {
@@ -10,27 +7,10 @@ public class LoginController {
 	
 	public LoginController(LoginPanel loginpanel) {
 		this.loginpanel = loginpanel;
-		loginpanel.btnSingInListener(new BtnSingInListener());
-	}
-	
-	class BtnSingInListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			/*
-			 * An Patithike to Singin Test
-			 */
-			String test = loginpanel.getUserType() + " " + loginpanel.getUserName() + " " +  loginpanel.getPass();
-			System.out.println(test);
-		}
 	}
 	
 	public String changePanel(){
 		return loginpanel.getUserType();
 	}
 	
-	/*
-	 * Test
-	public void panelListener(ActionListener act) {
-		loginpanel.btnSingIn.addActionListener(act);
-	}
-	*/
 }
